e26b13559594e07242e6f49ada99ed7d872c677f
Ozzie <ubalesaria@me.com>
2022-05-08 22:02:17 +0100

    change of display name

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Planet Simulation")
+pygame.display.set_caption("Ozzie's Planet Sim")
 
 def main():
     run = True
